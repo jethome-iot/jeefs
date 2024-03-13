93d86281dc86674add4eb402c53ea47ab80de48b
Viacheslav Bocharov <adeep@lexina.in>
2024-03-13 11:31:50 +0300

    fix add modules enum

--- include/jeefs.h
+++ include/jeefs.h
@@ -56,7 +56,7 @@ typedef struct {
     uint32_t crc32;
 } JEEFSFileHeader; // 24 bytes
 
-enum __attribute__ ((__packed__)) ModulesTypes
+enum __attribute__ ((__packed__)) JHModulesTypes
 {
     JHModule_Empty = 0,
     JHModule_WiFi_AP6255 = 1, //  AMPAK AP6255/6256
@@ -67,7 +67,7 @@ enum __attribute__ ((__packed__)) ModulesTypes
     JHModule_ZigBee_ZB3CX = 50,  // CC2538+CC2592
     JHModule_ZigBee_ZB6C = 51,   // CC2652P1
     JHModule_ZigBee_ZB4C = 52,   // EFR32MG21
-   };
+};
 
 #pragma pack(pop)
 
