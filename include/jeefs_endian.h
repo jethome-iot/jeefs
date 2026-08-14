94959538b240a4bc2232a5011743b25354053959
Viacheslav Bocharov <v@baodeep.com>
2026-08-14 13:48:32 +0300

    jeefs_endian: match repo style (4-space indent, Author line)
    .clang-format specifies IndentWidth 4; every other header carries an
    Author line. Bring the new header in line.

--- include/jeefs_endian.h
+++ include/jeefs_endian.h
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: (GPL-2.0+ or Apache-2.0)
 /*
  * Copyright (c) 2026 JetHome. All rights reserved.
+ * Author: Viacheslav Bocharov <v@baodeep.com>
  *
  * Little-endian field accessors for the wire format. Freestanding
  * (stdint only). Will fold into the port layer (#24) together with the
@@ -17,24 +18,24 @@ extern "C" {
 #endif
 
 static inline uint16_t jeefs_get_le16(const uint8_t *p) {
-  return (uint16_t)(p[0] | (p[1] << 8));
+    return (uint16_t)(p[0] | (p[1] << 8));
 }
 
 static inline uint32_t jeefs_get_le32(const uint8_t *p) {
-  return (uint32_t)p[0] | ((uint32_t)p[1] << 8) | ((uint32_t)p[2] << 16) |
-         ((uint32_t)p[3] << 24);
+    return (uint32_t)p[0] | ((uint32_t)p[1] << 8) | ((uint32_t)p[2] << 16) |
+           ((uint32_t)p[3] << 24);
 }
 
 static inline void jeefs_put_le16(uint8_t *p, uint16_t v) {
-  p[0] = (uint8_t)(v & 0xFF);
-  p[1] = (uint8_t)(v >> 8);
+    p[0] = (uint8_t)(v & 0xFF);
+    p[1] = (uint8_t)(v >> 8);
 }
 
 static inline void jeefs_put_le32(uint8_t *p, uint32_t v) {
-  p[0] = (uint8_t)(v & 0xFF);
-  p[1] = (uint8_t)((v >> 8) & 0xFF);
-  p[2] = (uint8_t)((v >> 16) & 0xFF);
-  p[3] = (uint8_t)((v >> 24) & 0xFF);
+    p[0] = (uint8_t)(v & 0xFF);
+    p[1] = (uint8_t)((v >> 8) & 0xFF);
+    p[2] = (uint8_t)((v >> 16) & 0xFF);
+    p[3] = (uint8_t)((v >> 24) & 0xFF);
 }
 
 #ifdef __cplusplus
