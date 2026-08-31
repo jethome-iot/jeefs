fcf98f4271c55caaf0d12d731e9b80fd6544bf0d
Viacheslav Bocharov <v@baodeep.com>
2026-08-31 18:19:17 +0300

    Release 0.7.1
    The timestamp semantics are stated everywhere they were implied: the
    serializer docstring, the format spec and a wire-locking test agree
    that timestamps are written verbatim and 0 is the not-provisioned
    placeholder state.

--- include/jeefs_version.h
+++ include/jeefs_version.h
@@ -4,10 +4,10 @@
 #ifndef JEEFS_VERSION_H
 #define JEEFS_VERSION_H
 
-#define JEEFS_VERSION "0.7.0"
+#define JEEFS_VERSION "0.7.1"
 #define JEEFS_VERSION_MAJOR 0
 #define JEEFS_VERSION_MINOR 7
-#define JEEFS_VERSION_PATCH 0
+#define JEEFS_VERSION_PATCH 1
 
 #ifdef __cplusplus
 extern "C" {
